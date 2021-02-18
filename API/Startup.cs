a7311374080403a88464caf3387a4493ef244556
Ciro Martin <cimartin@santandertecnologia.com.ar>
2021-02-18 18:44:19 -0300

    Se agrega el client-app consumiendo API endpoint

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,17 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("Default"));
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,9 +61,11 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
