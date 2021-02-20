035a13e9610d740a77154ce93874cfb9f421ebb3
Ciro Martin <cimartin@santandertecnologia.com.ar>
2021-02-20 08:01:32 -0300

    delete en la api desde el cliente

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -68,7 +68,11 @@ function App() {
   }
 
   function handleDeleteActivity(id: string) {
-    setActivities([...activities.filter((a) => a.id !== id)]);
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter((a) => a.id !== id)]);
+      setSubmitting(false);
+    });
   }
 
   if (loading) return <LoadingComponent content='Loading app'/>
