11c4812179bdf07da53c0722b179bfb3d272ec2e
Ciro Martin <cimartin@santandertecnologia.com.ar>
2021-03-10 07:20:03 -0300

    se configuro los distintos tipos de errores en el cliente

---
+++ client-app/src/features/errors/ValidationErrors.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Message } from 'semantic-ui-react'
+
+interface Props {
+    errors: string[];
+}
+
+export default function ValidationErrors({errors}: Props) {
+    return (
+        <Message error>
+            {errors && (
+                <Message.List>
+                    {errors.map((error: any, i) => (
+                        <Message.Item key={i}>{error}</Message.Item>
+                    ))}
+                </Message.List>
+            )}
+        </Message>
+    )
+}
